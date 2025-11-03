f9f6cb5788636c73c9983ddfb9355ffd7f60797c
AliRadwan1 <aliradwan792@gmail.com>
2025-11-03 20:12:38 +0200

    add wroking file for GUI

---
+++ GUI/20230198_20231110_20230150_20230354_20230655_AI5.java
@@ -0,0 +1,11 @@
+/*
+* Group Members:
+* Ali Radwan Farouk    20231110
+* Adel Hefny           20230198
+* Ziad Salama          20230150
+* Mohamed Mahmoud      20230354
+* Asser Ahmed          20230655
+* 
+* Section: AI S5
+* Submission to TA: Mena Asfour
+ */
